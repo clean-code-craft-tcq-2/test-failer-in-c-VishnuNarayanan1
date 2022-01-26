87740695b98194eeea0c0f58690cc8601717011d
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-26 11:04:36 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -2,8 +2,10 @@
 #include <assert.h>
 
 int alertFailureCount = 0;
+int UpperThresholdLimitInCelcius = 200;
+int LowerThresholdLimitInCelcius = 100;
 
-int networkAlertStub(float celcius) {
+int networkAlert(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
@@ -11,9 +13,9 @@ int networkAlertStub(float celcius) {
     return 200;
 }
 
-void alertInCelcius(float farenheit) {
+void alertInCelcius(float farenheit, int (*Fn_Ptr_NetworkAlert)(float) {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
+    int returnCode = Fn_Ptr_NetworkAlert(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -23,9 +25,35 @@ void alertInCelcius(float farenheit) {
     }
 }
 
+                    // Test environment 
+                    
+int networkAlertStub(float celcius) 
+{
+    int alertCode = 0;
+    if((celcius >= UpperThresholdLimitInCelcius) || (celcius <= LowerThresholdLimitInCelcius)) && (celcius > 0))
+    {
+        printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+        alertCode = 500;
+    } 
+    else if ((celcius < UpperThresholdLimitInCelcius) && (celcius > LowerThresholdLimitInCelcius)))
+    {
+        alertCode = 200;
+    }
+    else
+    {
+        alertCode = 0xff // Invalid use case
+    }
+    return alertCode;
+}
+                    
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
+    int (*Fn_Ptr_NetworkAlert)(float) = networkAlertStub;
+    alertInCelcius(400.5,Fn_Ptr_NetworkAlert);
+    alertInCelcius(303.6,Fn_Ptr_NetworkAlert);
+    alertInCelcius(50,Fn_Ptr_NetworkAlert);
+    alertInCelcius(1000,Fn_Ptr_NetworkAlert);
+    alertInCelcius(0,Fn_Ptr_NetworkAlert);
+    assert(2,alertFailureCount);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
